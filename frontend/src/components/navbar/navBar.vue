3c577ae2dbd89896dfd0a64e2715cb1a199d6f80
Magnus Eik <mageik@stud.ntnu.no>
2025-03-27 14:55:10 +0100

    Remove standard vue content

--- frontend/src/components/navbar/navBar.vue
+++ frontend/src/components/navbar/navBar.vue
@@ -3,9 +3,29 @@
 </script>
 
 <template>
+  <header class="nav">
+    <div class="logo">
+      <RouterLink to="/browse"></RouterLink>
+      </div>
+    <nav>
+      <RouterLink to="Home"></RouterLink>
+    </nav>
 
+  </header>
 </template>
 
 <style scoped>
-
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: white;
+}
+.nav .logo a {
+  font-weight: bold;
+  color: black;
+}
+nav a {
+  color: #333;
+}
 </style>
