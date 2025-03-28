8dbe3d5bdded086091999d69f9713d7a7c48f45c
Magnus Eik <mageik@stud.ntnu.no>
2025-03-28 17:32:36 +0100

    feature(registration): add fields and button to RegistrationForm.vue

--- frontend/src/components/login/RegistrationForm.vue
+++ frontend/src/components/login/RegistrationForm.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const username = ref('')
+const email = ref('')
+const password = ref('')
+const firstName = ref('')
+const lastName = ref('')
+const birthDate = ref('')
+
 
 </script>
 
 <template>
-
+  <button type="submit">{{ 'REGISTRATION_BUTTON' }}}</button>
 </template>
 
