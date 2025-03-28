91fda4f0097b36f67369dbe79232e7c19c115d9a
Håvard Daleng <hjdaleng@stud.ntnu.no>
2025-03-28 13:42:19 +0100

    docs: Add documentation to new user-related backend classes.

--- backend/src/main/java/stud/ntnu/backend/data/UserDto.java
+++ backend/src/main/java/stud/ntnu/backend/data/UserDto.java
@@ -5,14 +5,36 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import stud.ntnu.backend.model.User;
 
+
+/**
+ * <h2> Public-facing user data object. </h2>
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
+
+  /**
+   * <h3> The id of the user. </h3>
+   */
   private Long id;
+
+  /**
+   * <h3> The username of the user. </h3>
+   */
   private String username;
+
+  /**
+   * <h3> The email of the user. </h3>
+   */
   private String email;
 
+  /**
+   * <h3> Create a UserDto from a User object. </h3>
+   *
+   * @param user The user object.
+   * @return The UserDto object.
+   */
   public static UserDto from(User user) {
     return new UserDto(user.getId(), user.getUsername(), user.getEmail());
   }
