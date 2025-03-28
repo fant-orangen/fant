e61d61398dbc024a83a9e80f5b4281797ae19487
Magnus Eik <mageik@stud.ntnu.no>
2025-03-28 17:12:11 +0100

    bug(registration): moved code from RegistrationForm.vue to RegistrationView.vue

--- frontend/src/views/RegistrationView.vue
+++ frontend/src/views/RegistrationView.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-
+import RegistrationForm from "@/components/login/RegistrationForm.vue";
 </script>
 
 <template>
-
+  <main class="registration-view">
+    <h1>{{ $t('REGISTRATION_HEADER') }}</h1>
+    <RegistrationForm />
+  </main>
 </template>
-
-<style scoped>
-
-</style>
