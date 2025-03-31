9662441dcc272e20d1862b6494226d3220987358
Nicklas Tufteland <nicklapt@stud.ntnu.no>
2025-03-31 17:37:53 +0200

    feat: added the itemPreview component to homeview class.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,19 +1,40 @@
-<script setup lang="ts">
-import CategoryGrid from "@/components/category/categoryGrid.vue";
-import {useRouter} from "vue-router";
-
-const router = useRouter();
-
-function onCategoryClick(categoryKey: string) {
-  router.push({ name: 'category', params: { categoryKey } })
-}
-</script>
-
 <template>
   <section class="homepage">
     <h1>{{ $t('APP_WELCOME') }}</h1>
     <p>Markedsplass for salg og kjøp av varer</p>
-    <!-- Listen for 'select' event -->
-    <CategoryGrid @select="onCategoryClick"/>
+    <CategoryGrid @select="onCategoryClick" />
+    <div class="item-previews thumbnail-container">
+      <ItemPreview v-for="item in items" :key="item.id" :item="item" />
+    </div>
   </section>
 </template>
+
+<script setup lang="ts">
+import CategoryGrid from "@/components/category/categoryGrid.vue";
+import ItemPreview from "@/components/item/itemPreview.vue";
+import { useRouter } from "vue-router";
+import { ref, onMounted } from 'vue';
+import { fetchPreviewItems } from '@/services/itemService/itemPreviewService';
+import type { ItemPreviewType } from '@/models/Item';
+
+const router = useRouter();
+const items = ref<ItemPreviewType[]>([]);
+
+function onCategoryClick(categoryKey: string) {
+  router.push({ name: 'category', params: { categoryKey } });
+}
+
+onMounted(async () => {
+  try {
+    items.value = await fetchPreviewItems();
+  } catch (error) {
+    console.error('Error fetching items:', error);
+  }
+});
+</script>
+<style>
+@import '@/assets/styles/responsiveStyles.css'; /* This will still be global */
+</style>
+<style scoped>
+
+</style>
