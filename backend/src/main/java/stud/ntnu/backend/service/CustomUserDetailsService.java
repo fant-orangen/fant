4ba237cf84a0e7d627475f3921e23e29c1eac870
Håvard Daleng <hjdaleng@stud.ntnu.no>
2025-03-31 13:39:18 +0200

    feat: Add to CustomUserDetailsService and implement proper getToken program chain.

--- backend/src/main/java/stud/ntnu/backend/service/CustomUserDetailsService.java
+++ backend/src/main/java/stud/ntnu/backend/service/CustomUserDetailsService.java
@@ -1,10 +1,13 @@
 package stud.ntnu.backend.service;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import stud.ntnu.backend.model.User;
 import stud.ntnu.backend.repository.UserRepository;
@@ -16,12 +19,15 @@ import java.util.Collections;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
-
+        String passwordHash = passwordEncoder.encode("password123");
+        logger.info("Hash of password123: " + passwordHash);
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPasswordHash(),
