84acdd88ddc5c006876aba0272911872c15a4d93
Nicklas Tufteland <nicklapt@stud.ntnu.no>
2025-04-01 10:29:14 +0200

    feat: Adjust image gallery to show page number.

--- frontend/src/components/show/ImageGallery.vue
+++ frontend/src/components/show/ImageGallery.vue
@@ -1,24 +1,25 @@
 <template>
   <div class="gallery-container">
-    <button class="nav-button left" @click="prevPage" :disabled="currentPage === 0">&#9664;</button>
     <div class="gallery">
+      <button class="nav-button left" @click="prevPage" :disabled="currentPage === 0">&#9664;</button>
       <img v-for="(image, index) in paginatedImages" :key="index" :src="image" :alt="`Image ${index + 1}`" class="gallery-image" />
+      <button class="nav-button right" @click="nextPage" :disabled="currentPage === totalPages - 1">&#9654;</button>
+      <div class="page-info">Page {{ currentPage + 1 }} of {{ totalPages }}</div>
     </div>
-    <button class="nav-button right" @click="nextPage" :disabled="currentPage === totalPages - 1">&#9654;</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, computed, defineProps } from 'vue';
 
-const props = defineProps<{ imageUrls: string[] }>();
+const props = defineProps<{ imageUrls: string[], imagesPerPage?: number }>();
 const currentPage = ref(0);
-const imagesPerPage = 1;
+const imagesPerPage = computed(() => props.imagesPerPage || 1);
 
-const totalPages = computed(() => Math.ceil(props.imageUrls.length / imagesPerPage));
+const totalPages = computed(() => Math.ceil(props.imageUrls.length / imagesPerPage.value));
 const paginatedImages = computed(() => {
-  const start = currentPage.value * imagesPerPage;
-  return props.imageUrls.slice(start, start + imagesPerPage);
+  const start = currentPage.value * imagesPerPage.value;
+  return props.imageUrls.slice(start, start + imagesPerPage.value);
 });
 
 function prevPage() {
@@ -35,23 +36,50 @@ function nextPage() {
 </script>
 
 <style scoped>
-.gallery-container {
+.gallery {
   display: flex;
+  position: relative;
+  margin-top: 2rem;
+  width: 20vw;
+  height: 20vw;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+}
+
+.gallery-image {
+  object-fit: contain;
+  width: 100%;
+  height: 100%;
 }
 
 .nav-button {
-  background: none;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
   border: none;
-  font-size: 2rem;
+  font-size: 1rem;
   cursor: pointer;
-  z-index: 1;
-}
-
-.nav-button.left {
-  margin-right: 1vw;
+  padding: 0.5rem;
+  border-radius: 50%;
+  z-index: 2;
 }
 
 .nav-button.right {
-  margin-left: 1vw;
+  right: 1px;
+}
+
+.nav-button:disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+
+.page-info {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  background: rgba(255, 255, 255, 0.1);
+  z-index: 2;
 }
 </style>
