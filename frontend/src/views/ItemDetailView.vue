877f5eb0866224cdba39ec8078e2265acb5456d5
Nicklas Tufteland <nicklapt@stud.ntnu.no>
2025-04-01 10:03:15 +0200

    feat: add view and mock data to see item Detail component.

---
+++ frontend/src/views/ItemDetailView.vue
@@ -0,0 +1,24 @@
+<template>
+  <ItemDetail :item="item" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ItemDetail from '@/components/item/ItemDetail.vue';
+import type { ItemDetailsType } from '@/models/Item';
+
+// Example item data
+const item = ref<ItemDetailsType>({
+  id: 1,
+  title: 'Sample Item',
+  description: 'This is a sample item description.',
+  price: 100,
+  location: 'Sample Location',
+  contact: 'nic@ntnu.no',
+  imageUrls: [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmmKsSJQM7-pvAYClePZmpp6oLWKHCn4rujA&s',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmmKsSJQM7-pvAYClePZmpp6oLWKHCn4rujA&s',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmmKsSJQM7-pvAYClePZmpp6oLWKHCn4rujA&s'
+  ]
+});
+</script>
