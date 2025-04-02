4d43a9a04665055d42c0d2d21cddca32b703ff85
Håvard Daleng <hjdaleng@stud.ntnu.no>
2025-04-02 12:46:12 +0200

    test: Add some test items via insert to data.sql.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -7,3 +7,9 @@ VALUES (2, 'jakob', 'jakob@mail.com', '$2a$10$9JB0yUlymHFA23jzoN9VWOPF6UIjLPCWFx
 
 INSERT INTO categories (name, description)
 VALUES ('Test', 'Test');
+
+INSERT INTO items (id, seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
+VALUES (1, 1, 2, 'Test', 'Test', 1.00, 'ACTIVE', NOW(), NOW());
+
+INSERT INTO items (id, seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
+VALUES (2, 1, 2, 'Test', 'Test', 1.00, 'ACTIVE', NOW(), NOW());
