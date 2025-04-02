11b84527a1075f49fe5bd30851d013cdc928f8f7
Magnus Eik <mageik@stud.ntnu.no>
2025-04-02 12:05:53 +0200

    feature(profile): add MapView.vue

---
+++ frontend/src/views/MapView.vue
@@ -0,0 +1,12 @@
+// Example: In src/views/HomeView.vue
+<template>
+  <div>
+    <h1>{{ $t('APP_WELCOME') }}</h1>
+    <MapComponent />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MapComponent from '@/components/map/MapComponent.vue'; // Import the map component
+
+</script>
