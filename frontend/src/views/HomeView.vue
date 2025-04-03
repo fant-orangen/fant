cb27201e8359721125b57871679cee8ca67e325c
Håvard Daleng <hjdaleng@stud.ntnu.no>
2025-04-03 13:32:59 +0200

    feat: Add empty onMounted function and new RecommendationService as well as Recommendation model.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -8,9 +8,15 @@
 <script setup lang="ts">
 import CategoryGrid from "@/components/category/categoryGrid.vue";
 import CategoryItemList from "@/components/item/CategoryItemList.vue";
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 const selectedCategoryId = ref<string | null>(null);
+
+onMounted(async () => {
+
+  // Fetch initial data or perform any setup needed when the component is mounted
+  // For example, you might want to fetch categories or items here
+});
 
 function onCategoryClick(categoryId: string) {
   selectedCategoryId.value = categoryId;
