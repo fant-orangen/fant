d4684371ac017278c22b7f3d229e680d4d24de9d
Håvard Daleng <hjdaleng@stud.ntnu.no>
2025-04-03 11:05:19 +0200

    fix: Remove dark mode setting because I can't seem to turn it off by changing system theme.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,4 +12,25 @@ INSERT INTO items (seller_id, category_id, brief_description, full_description, 
 VALUES (1, 1, 'Test', 'Test', 500.00, 'ACTIVE', NOW(), NOW());
 
 INSERT INTO items (seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
-VALUES (1, 1, 'Test', 'Test', 200.00, 'ACTIVE', NOW(), NOW());
+VALUES (1, 1, 'Lambi Deluxe', 'Test', 200.00, 'ACTIVE', NOW(), NOW());
+
+INSERT INTO items (seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
+VALUES (1, 1, 'Mills Majones', 'Test', 500.00, 'ACTIVE', NOW(), NOW());
+
+INSERT INTO items (seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
+VALUES (1, 1, 'Ny bil!!!!', 'Test', 100.00, 'ACTIVE', NOW(), NOW());
+
+INSERT INTO items (seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
+VALUES (1, 1, 'Vaskepulver med jordbærsmak', 'Et fantastisk vaskepulver med smak av jordbær! Mmm!', 240.00, 'ACTIVE', NOW(), NOW());
+
+INSERT INTO items (seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
+VALUES (2, 1, 'Dinosaurkjæledyr', 'Han heter Ronald.', 240.00, 'ACTIVE', NOW(), NOW());
+
+INSERT INTO items (seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
+VALUES (2, 1, 'Femten planker', 'Femten planker jeg ikke trenger. Høy sentimental verdi!', 15000.00, 'ACTIVE', NOW(), NOW());
+
+INSERT INTO items (seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
+VALUES (2, 1, 'Bringebærsyltetøy', 'Ikke så mye mer å si', 59.0, 'ACTIVE', NOW(), NOW());
+
+INSERT INTO items (seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
+VALUES (2, 1, 'Diskotek', 'Selger gammelt diskotek; familiebusiness.', 9000000.00, 'ACTIVE', NOW(), NOW());
