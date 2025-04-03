d826b48018ed46f33cf80db35eadab004414905c
Håvard Daleng <hjdaleng@stud.ntnu.no>
2025-04-03 11:23:00 +0200

    test: Add and successfully test images.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -34,3 +34,39 @@ VALUES (2, 1, 'Bringebærsyltetøy', 'Ikke så mye mer å si', 59.0, 'ACTIVE', N
 
 INSERT INTO items (seller_id, category_id, brief_description, full_description, price, status, published_at, updated_at)
 VALUES (2, 1, 'Diskotek', 'Selger gammelt diskotek; familiebusiness.', 9000000.00, 'ACTIVE', NOW(), NOW());
+
+-- Images for items
+
+-- Insert mock images for item 2 (Lambi Deluxe)
+INSERT INTO item_images (item_id, image_url, position)
+VALUES (2, 'https://i.ibb.co/wrBBYXmw/lambi.jpg', 0);
+
+-- Insert mock images for item 3 (Mills Majones)
+INSERT INTO item_images (item_id, image_url, position)
+VALUES (3, 'https://i.ibb.co/VYxptvrF/mills.jpg', 0);
+
+-- Insert mock images for item 4 (Ny bil!!!!)
+INSERT INTO item_images (item_id, image_url, position)
+VALUES (4, 'https://i.ibb.co/FkRNqvG3/lambo.jpg', 0);
+
+-- Insert mock images for item 5 (Vaskepulver med jordbærsmak)
+INSERT INTO item_images (item_id, image_url, position)
+VALUES (5, 'https://i.ibb.co/HfHbnGNs/vaskepulver.webp', 0);
+
+-- Insert mock images for item 6 (Dinosaurkjæledyr)
+INSERT INTO item_images (item_id, image_url, position)
+VALUES (6, 'https://i.ibb.co/h1922gsJ/velociraptor.webp', 0);
+
+-- Insert mock images for item 7 (Femten planker)
+INSERT INTO item_images (item_id, image_url, position)
+VALUES (7, 'https://i.ibb.co/Y5mZ4DJ/15-planks.jpg', 0);
+
+
+-- Insert mock images for item 8 (Bringebærsyltetøy)
+INSERT INTO item_images (item_id, image_url, position)
+VALUES (8, 'https://i.ibb.co/ktPs8NS/syltetoy.png', 0);
+
+-- Insert mock images for item 9 (Diskotek)
+INSERT INTO item_images (item_id, image_url, position)
+VALUES (9, 'https://i.ibb.co/HTFJ6ky9/discoteque.jpg', 0);
+
