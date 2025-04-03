dc73006e17772e499d019770317e7ddc945c8f20
Magnus Eik <mageik@stud.ntnu.no>
2025-04-03 13:46:35 +0200

    feature(profile): create MapComponent.spec.ts

---
+++ frontend/src/components/__tests__/stores/MapComponent.spec.ts
@@ -0,0 +1,5 @@
+import { mount } from '@vue/test-utils';
+import MapComponent from "@/components/map/MapComponent.vue";
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import L from 'leaflet';
+
