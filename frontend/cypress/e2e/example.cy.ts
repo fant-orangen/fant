e74818804ca203d8295cbc3f707101b0de195033
Magnus Eik <mageik@stud.ntnu.no>
2025-04-10 17:48:00 +0200

    test(searching): delete example test

--- frontend/cypress/e2e/example.cy.ts
+++
@@ -1,8 +0,0 @@
-// https://on.cypress.io/api
-
-describe('My First Test', () => {
-  it('visits the app preview url', () => {
-    cy.visit('/')
-    cy.get('section.homepage').should('exist')
-  })
-})
